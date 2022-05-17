69b87cb645d9d5213d81980b91f6c1ac5eeb2f90
Miloslav Jezek <64562000+milojezek@users.noreply.github.com>
2022-05-17 21:49:56 +0200

    Use List.map() instead of for-loop

--- solutions/8kyu/Beginner - Lost Without a Map/Maps.kt
+++ solutions/8kyu/Beginner - Lost Without a Map/Maps.kt
@@ -1,7 +1,3 @@
 fun maps(x: IntArray): IntArray {
-    for (index in 0..x.size - 1) {
-        x[index] = x[index] * 2
-    }
-    
-    return x
+    return x.toList().map { it * 2}.toIntArray()
 }
