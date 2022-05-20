7a27c4d7fef7608edc6f56ab601e7a8979c0e289
Miloslav Jezek <64562000+milojezek@users.noreply.github.com>
2022-05-20 22:10:44 +0200

    Update HexStringToRGB.kt

--- solutions/5kyu/Convert A Hex String To RGB/HexStringToRGB.kt
+++ solutions/5kyu/Convert A Hex String To RGB/HexStringToRGB.kt
@@ -1,7 +1,5 @@
-fun hexStringToRGB(hexString: String): RGB {
-    val r = hexString.slice(1..2).toInt(16)
-    val g = hexString.slice(3..4).toInt(16)
-    val b = hexString.slice(5..6).toInt(16)
-    val rgb = RGB(r, g, b)
-    return rgb  
-}
+fun hexStringToRGB(hexString: String): RGB = RGB(
+    hexString.slice(1..2).toInt(16),
+    hexString.slice(3..4).toInt(16),
+    hexString.slice(5..6).toInt(16)
+)
