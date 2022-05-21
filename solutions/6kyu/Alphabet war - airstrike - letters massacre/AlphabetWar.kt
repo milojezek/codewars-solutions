7824194e016c50143e53b15608c290bf6d922b63
Miloslav Jezek <64562000+milojezek@users.noreply.github.com>
2022-05-21 23:52:39 +0200

    Create AlphabetWar.kt

---
+++ solutions/6kyu/Alphabet war - airstrike - letters massacre/AlphabetWar.kt
@@ -0,0 +1,26 @@
+fun alphabetWar(input: String): String {
+    val pattern = "\\w[*]\\w".toRegex()
+    val pattern1 = "[*]\\w".toRegex()
+    val pattern2 = "\\w[*]".toRegex()
+    val fight = input.replace(pattern, "___").replace(pattern1, "__").replace(pattern2, "__")
+    
+    var leftSideScore = 0
+    var rightSideScore = 0
+
+    val leftSidePower = mapOf('w' to 4, 'p' to 3, 'b' to 2, 's' to 1)
+    val rightSidePower = mapOf('m' to 4, 'q' to 3, 'd' to 2, 'z' to 1)
+    
+    for (letter in fight) {
+        when (letter) {
+        in leftSidePower.keys -> leftSideScore += leftSidePower.getValue(letter)
+        in rightSidePower.keys -> rightSideScore += rightSidePower.getValue(letter)
+        else -> continue
+        }
+    }
+    
+    return when {
+        leftSideScore > rightSideScore -> "Left side wins!"
+        leftSideScore < rightSideScore -> "Right side wins!"
+        else -> "Let's fight again!"
+    }
+}
