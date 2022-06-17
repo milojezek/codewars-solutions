87803ac3ec5c4a431bcc3d65853698861fbd5c76
Miloslav Jezek <64562000+milojezek@users.noreply.github.com>
2022-06-17 18:36:33 +0200

    Update FizzBuzz.kt

--- solutions/7kyu/Fizz Buzz/FizzBuzz.kt
+++ solutions/7kyu/Fizz Buzz/FizzBuzz.kt
@@ -1,12 +1,12 @@
 fun fizzBuzz(n: Int): Array<String> {
     val fizzBuzzList: MutableList<String> = mutableListOf()
     for (number in 1..n) {
-        when {
-            number % 5 == 0 && number % 3 == 0 -> fizzBuzzList.add("FizzBuzz")
-            number % 5 == 0 -> fizzBuzzList.add("Buzz")
-            number % 3 == 0 -> fizzBuzzList.add("Fizz")
-            else -> fizzBuzzList.add(number.toString())
-        }
+        fizzBuzzList.add(when {
+            number % 5 == 0 && number % 3 == 0 -> "FizzBuzz"
+            number % 5 == 0 -> "Buzz"
+            number % 3 == 0 -> "Fizz"
+            else -> number.toString()
+        })
     }
     
     return fizzBuzzList.toTypedArray()
